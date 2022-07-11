7632d53ee114917ba4c6c99265e9dfb127a1d0a4
Billy Cole <billycole@billys-mbp.mynetworksettings.com>
2022-07-11 11:05:20 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import Lottie from "lottie-react";
+
+import './reset.css';
 import './App.css';
+import constructionAnimation from "./construction.json";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <div className="lottie">
+        <Lottie animationData={constructionAnimation} loop={true} />
+      </div>
+      <h1><span>Under Construction</span> &#x1F477;</h1>
+    </main>
   );
 }
 
